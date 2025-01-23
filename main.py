bd554d0a9cf38e1ac6fce75f7f8c0b1d967596ed
Samuel Jonathan <107603784+saamukai@users.noreply.github.com>
2025-01-23 15:17:30 -0300

    :recycle: refactor: Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import discord
 from discord.ext import commands
 
-from concursos_brasil import concursos_brasil
-from lock_unlock_chat import lock_unlock_vip
-from move_user import move_users
+from scrapy_contests import alert_new_contest
+from camroom_automod import move_users
 
-TOKEN = 'TOKEN_BOT'
-GUILD_ID = int('ID_SERVER')
-
-
+TOKEN = 'BOT_TOKEN_DISCORD'
+GUILD_ID = int('GUILD_ID')
 INTENTS = discord.Intents.default()
 BOT = commands.Bot(command_prefix='!e', intents=INTENTS)
-
 
 @BOT.event
 async def on_ready():
     print(f'Logged in as {BOT.user}')
-    concursos_brasil.start(BOT, GUILD_ID)
+    alert_new_contest.start(BOT, GUILD_ID)  
     move_users.start(BOT, GUILD_ID)
-    lock_unlock_vip.start(BOT, GUILD_ID)
-
 
 if __name__ == '__main__':
     BOT.run(TOKEN)
